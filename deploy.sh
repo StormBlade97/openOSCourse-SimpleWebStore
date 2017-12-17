a39e3097e0bed46c27dd6ec046d917d6cc544b92
Christian Tschoepe <tschoepe.christian@gmail.com>
2017-12-17 23:05:28 +0200

    no sudo

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,5 @@ echo "Installing all dependency for backend"
 npm install
 echo "Building latest backend"
 npm run build
-echo "Building front end"
-cd ./client-simple
-npm install
-npm run build
-cd ..
 echo "Done. Now running server"
-sudo npm run start
+npm run start
